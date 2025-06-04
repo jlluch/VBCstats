d6e9619fba661c2cebd4060c103c8ae1e2891296
jlluch <jlluch@ai2.upv.es>
2025-06-04 11:26:36 +0200

    Update VBCstats.py

--- VBCstats.py
+++ VBCstats.py
@@ -8,16 +8,24 @@ import pandas as pd
 from st_pages import get_nav_from_toml
 import streamlit.components.v1 as components
 
-ga_code = """
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-HTHR3HRXS1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-HTHR3HRXS1');
+sc_code = """
+<!-- Default Statcounter code for VBC Stats
+https://vbcstats.streamlit.app/ -->
+<script type="text/javascript">
+var sc_project=13141410; 
+var sc_invisible=1; 
+var sc_security="8c3d850a"; 
 </script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js"
+async></script>
+<noscript><div class="statcounter"><a title="Web Analytics
+Made Easy - Statcounter" href="https://statcounter.com/"
+target="_blank"><img class="statcounter"
+src="https://c.statcounter.com/13141410/0/8c3d850a/1/"
+alt="Web Analytics Made Easy - Statcounter"
+referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
+<!-- End of Statcounter Code -->
 """
 
 # Configuración de la página
@@ -28,7 +36,7 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-components.html(ga_code, height=0)
+components.html(sc_code, height=0)
 
 st.markdown("""
 <style>
