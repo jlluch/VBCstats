35c93b32d25c6438c78c9f7f52d82ad138946c5b
jlluch <jlluch@ai2.upv.es>
2025-06-19 11:45:14 +0200

    Update 5_CopaDelRey.py

--- VBCpages/5_CopaDelRey.py
+++ VBCpages/5_CopaDelRey.py
@@ -1022,8 +1022,7 @@ elif marco == "Estadísticas contra un rival":
     st.write("Estadísticas")
     st.dataframe(stats_total, height=565, column_config={
             stats_total.index.name: st.column_config.TextColumn(width="medium"),
-            stats_total.Total: st.column_config.TextColumn(width="small")
-        })
+            "Total": st.column_config.TextColumn(width="small")})
 
 elif marco == "Entrenadores":
     # Crear un marco para mostrar los entrenadores
