92a0faf14c27ca4e40764278ea8e2ebc5f2fc067
jlluch <jlluch@ai2.upv.es>
2025-07-07 11:32:44 +0200

    Update 6_Totales.py

--- VBCpages/6_Totales.py
+++ VBCpages/6_Totales.py
@@ -5,111 +5,23 @@
 # """
 import streamlit as st
 import pandas as pd
-import ast
-from VBCpages.encrypt_utils import decrypt_csv_file
-
-def load_encrypted_data(file_path):
-    try:
-        # Get decryption key from secrets
-        decryption_key = st.secrets["data_encryption"]["key"]
-        return decrypt_csv_file(file_path, decryption_key)
-    except Exception as e:
-        st.error(f"Error accessing data: {str(e)}")
-        return None
-
-#Crear una función para cargar los datos de cada competición, de partidos y de jugadores
-#Hacer cache de los datos para que no se carguen cada vez que se actualiza la página
-
-# Crear una columna con el partido
-def create_partido_column(df):
-    df['Partido'] = df.apply(lambda x: f"VBC - {x['Equipo Rival']}" if x['VBC Local'] else f"{x['Equipo Rival']} - VBC", axis=1)
-    return df
-
-
 @st.cache_data 
 def load_data():
-    path = r"Data/"
+    df_players_VBC = st.session_state.df_players_VBC
+    df_coaches_VBC = st.session_state.df_coaches_VBC
+    df_players_Total = st.session_state.df_players_Total
+    df_games_Total = st.session_state.df_games_Total
     
-    df_players_VBC = pd.read_csv(path+"jugadores_VBC.csv")
-    
-    file_path = path+"estadisticas_jugadores_VBC_ACB.csv.enc"
-    df_players_ACB = load_encrypted_data(file_path)
-    file_path = path+"estadisticas_jugadores_VBC_Eurocup.csv.enc"
-    df_players_Eurocup = load_encrypted_data(file_path)
-    file_path = path+"estadisticas_jugadores_VBC_Euroleague.csv.enc"
-    df_players_Euroleague = load_encrypted_data(file_path)
-    file_path = path+"estadisticas_jugadores_VBC_Saporta.csv.enc"
-    df_players_Saporta = load_encrypted_data(file_path)
-    file_path = path+"estadisticas_jugadores_VBC_CopaRey.csv.enc"
-    df_players_CopaRey = load_encrypted_data(file_path)    
-    df_players_Eurocup['ID Jugador'] = df_players_Eurocup['ID Jugador'].apply(lambda x: df_players_VBC[df_players_VBC['ID Eurocup'] == x]['ID ACB'].values[0] if not df_players_VBC[df_players_VBC['ID Eurocup'] == x].empty else x)
-    df_players_Euroleague['ID Jugador'] = df_players_Euroleague['ID Jugador'].apply(lambda x: df_players_VBC[df_players_VBC['ID Euroleague'] == x]['ID ACB'].values[0] if not df_players_VBC[df_players_VBC['ID Euroleague'] == x].empty else x)
-    df_players_Saporta['ID Jugador'] = df_players_Saporta['ID Jugador'].apply(lambda x: df_players_VBC[df_players_VBC['ID Saporta'] == x]['ID ACB'].values[0] if not df_players_VBC[df_players_VBC['ID Saporta'] == x].empty else x)
-
-
-    file_path = path+"estadisticas_partidos_VBC_ACB.csv.enc"
-    df_games_ACB = load_encrypted_data(file_path)
-    file_path = path+"estadisticas_partidos_VBC_Eurocup.csv.enc"
-    df_games_Eurocup = load_encrypted_data(file_path)
-    file_path = path+"estadisticas_partidos_VBC_Euroleague.csv.enc"
-    df_games_Euroleague = load_encrypted_data(file_path)
-    file_path = path+"estadisticas_partidos_VBC_Saporta.csv.enc"
-    df_games_Saporta = load_encrypted_data(file_path)
-    file_path = path+"estadisticas_partidos_VBC_CopaRey.csv.enc"
-    df_games_CopaRey = load_encrypted_data(file_path)
-    
-    # Entrenadores de VBC
-    df_coaches_VBC = df_games_ACB[['ID Entrenador VBC', 'Entrenador VBC']].drop_duplicates()
-    
-    # Convertir las columnas de fecha a datetime
-    df_games_ACB['Fecha'] = pd.to_datetime(df_games_ACB['Fecha'], format='%d/%m/%Y')
-    df_games_Eurocup['Fecha'] = pd.to_datetime(df_games_Eurocup['Fecha'], format='%Y-%m-%d')
-    df_games_Euroleague['Fecha'] = pd.to_datetime(df_games_Euroleague['Fecha'], format='%Y-%m-%d')
-    df_games_Saporta['Fecha'] = pd.to_datetime(df_games_Saporta['Fecha'], format='%d.%m.%y')
-    df_games_CopaRey['Fecha'] = pd.to_datetime(df_games_CopaRey['Fecha'], format='%d/%m/%Y')
-
-    # Convertit ID Temporada a int
-    df_games_ACB['ID Temporada'] = df_games_ACB['ID Temporada'].astype(int)
-    df_games_Eurocup['ID Temporada'] = df_games_Eurocup['ID Temporada'].astype(int)
-    df_games_Euroleague['ID Temporada'] = df_games_Euroleague['ID Temporada'].astype(int)
-    df_games_Saporta['ID Temporada'] = df_games_Saporta['ID Temporada'].astype(int)
-    df_games_CopaRey['ID Temporada'] = df_games_CopaRey['ID Temporada'].astype(int)
-
-    # Aplicar la función a cada dataframe de partidos
-    df_games_ACB = create_partido_column(df_games_ACB)
-    df_games_Eurocup = create_partido_column(df_games_Eurocup)
-    df_games_Euroleague = create_partido_column(df_games_Euroleague)
-    df_games_Saporta = create_partido_column(df_games_Saporta)
-    df_games_CopaRey = create_partido_column(df_games_CopaRey)
-
-    # Crer una columna con el enlace a la página del partido
-    df_games_CopaRey['Enlace'] = 'https://www.acb.com/partido/estadisticas/id/' + df_games_CopaRey['ID Partido'].astype(str)
-    df_games_ACB['Enlace'] = 'https://www.acb.com/partido/estadisticas/id/' + df_games_ACB['ID Partido'].astype(str)
-    df_games_Eurocup['Enlace'] = df_games_Eurocup['ID Partido']
-    df_games_Euroleague['Enlace'] = df_games_Euroleague['ID Partido']
-    df_games_Saporta['Enlace'] = 'https://www.fibaeurope.com/' + df_games_Saporta['ID Partido'].astype(str)
-
-    # Crear una columna con la diferencia de puntos
-    df_games_ACB['Diferencia'] = df_games_ACB['Puntos VBC'] - df_games_ACB['Puntos Rival']
-    df_games_Eurocup['Diferencia'] = df_games_Eurocup['Puntos VBC'] - df_games_Eurocup['Puntos Rival']
-    df_games_Euroleague['Diferencia'] = df_games_Euroleague['Puntos VBC'] - df_games_Euroleague['Puntos Rival']
-    df_games_Saporta['Diferencia'] = df_games_Saporta['Puntos VBC'] - df_games_Saporta['Puntos Rival']
-    df_games_CopaRey['Diferencia'] = df_games_CopaRey['Puntos VBC'] - df_games_CopaRey['Puntos Rival']
-    
-    
-    return df_players_CopaRey, df_games_CopaRey, df_players_ACB, df_games_ACB, df_players_Eurocup, df_games_Eurocup, df_players_Euroleague, df_games_Euroleague, df_players_Saporta, df_games_Saporta, df_players_VBC, df_coaches_VBC
+    return df_players_VBC, df_coaches_VBC, df_games_Total, df_players_Total
     
 
 #Cargar todos los datos
-df_players_CopaRey, df_games_CopaRey, df_players_ACB, df_games_ACB, df_players_Eurocup, df_games_Eurocup, df_players_Euroleague, df_games_Euroleague, df_players_Saporta, df_games_Saporta, df_players_VBC, df_coaches_VBC = load_data()
+df_players_VBC, df_coaches_VBC, df_games_Total, df_players_Total = load_data()
 
 # Temporadas de cada competición
 Saporta = [1998,1999,2000,2001]
 Euroleague = [2003, 2010, 2014, 2017, 2019, 2020, 2022, 2023 ]
 Eurocup = [2002, 2004, 2007, 2008, 2009, 2011, 2012, 2013, 2014, 2015, 2016, 2018, 2021, 2024]
-
-df_games_Total = pd.concat([df_games_ACB, df_games_Eurocup, df_games_Euroleague, df_games_Saporta, df_games_CopaRey], ignore_index=True)
-df_players_Total = pd.concat([df_players_ACB, df_players_Eurocup, df_players_Euroleague, df_players_Saporta, df_players_CopaRey], ignore_index=True)
 
 # Crear un diccionario que mapee ID Jugador a Nombre a partir de df_players_VBC la clave se llama ID Jugador
 player_names = df_players_VBC.set_index('ID ACB')['Nombre ACB'].to_dict()
@@ -262,7 +174,7 @@ if marco == "Totales equipo":
 elif marco == "Estadísticas de una temporada conjunta":
     
     #Seleccionar una temporada, ordenar las temporadas de mayor a menor
-    season = st.selectbox("Selecciona una temporada", df_games_ACB['ID Temporada'].sort_values(ascending=False).unique())
+    season = st.selectbox("Selecciona una temporada", df_games_Total['ID Temporada'].sort_values(ascending=False).unique())
     
     # Filtrar partidos de una temporada de ACB, Copa del Rey y la competición eurpea jugada en esa temporada
     season_games = df_games_Total[df_games_Total['ID Temporada'] == season]
@@ -462,7 +374,7 @@ elif marco == "Estadísticas de una temporada conjunta":
 elif marco == "Estadísticas jugadores de una temporada conjunta":
     
     #Seleccionar una temporada, ordenar las temporadas de mayor a menor
-    season = st.selectbox("Selecciona una temporada", df_games_ACB['ID Temporada'].sort_values(ascending=False).unique())
+    season = st.selectbox("Selecciona una temporada", df_games_Total['ID Temporada'].sort_values(ascending=False).unique())
 
     # Filtrar partidos de una temporada de ACB, Copa del Rey y la competición eurpea jugada en esa temporada
     season_games = df_games_Total[df_games_Total['ID Temporada'] == season]
@@ -1337,7 +1249,7 @@ elif marco == "Récords equipo":
     max10_t2p = df_games_Total.sort_values(by=['T2% VBC','T2a VBC'],ascending=False).head(lh)
     #Seleccionar las columnas a mostrar
     columns_to_show = ['ID Temporada', 'Jornada', 'Fecha', 'Partido', 'T2% VBC', 'T2a VBC', 'Enlace']
-    st.write("Récords en porcentaje de tiros de 2 puntos")
+    st.write("Récords en porcentaje de 2 puntos")
     st.dataframe(max10_t2p[columns_to_show], hide_index=True, column_config={
         "ID Temporada": st.column_config.NumberColumn(width="small"),
         "Jornada": st.column_config.NumberColumn(width="small"),
@@ -1352,7 +1264,7 @@ elif marco == "Récords equipo":
     max10_t3p = df_games_Total.sort_values(by=['T3% VBC','T3a VBC'],ascending=False).head(lh)
     #Seleccionar las columnas a mostrar
     columns_to_show = ['ID Temporada', 'Jornada', 'Fecha', 'Partido', 'T3% VBC', 'T3a VBC', 'Enlace']
-    st.write("Récords en porcentaje de tiros de 3 puntos")
+    st.write("Récords en porcentaje de 3 puntos")
     st.dataframe(max10_t3p[columns_to_show], hide_index=True, column_config={
         "ID Temporada": st.column_config.NumberColumn(width="small"),
         "Jornada": st.column_config.NumberColumn(width="small"),
