85312f8f161883d4c6e32fc13c0011afdbe772ea
jlluch <jlluch@ai2.upv.es>
2025-07-08 18:17:34 +0200

    Update VBCstats.py

--- VBCstats.py
+++ VBCstats.py
@@ -188,6 +188,8 @@ nav = get_nav_from_toml(path=".streamlit/pages.toml")
 # Crea la navegación
 pg = st.navigation(nav)
 
+st.write(pg) 
+
 # --- CONTADOR DE VISITAS POR PÁGINA ---
 def contar_visita_google_sheets(nombre_pagina):
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
@@ -207,7 +209,7 @@ def contar_visita_google_sheets(nombre_pagina):
     sheet.append_row([nombre_pagina, 1])
     return 1
 # ✅ Obtener nombre de la página seleccionada
-nombre_pagina = pg.name
+nombre_pagina = '1.ACB'
 
 # Solo contar una vez por carga
 if "pagina_contada" not in st.session_state or st.session_state.pagina_contada != nombre_pagina:
